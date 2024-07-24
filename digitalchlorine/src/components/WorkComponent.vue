1e4c5436bb34d4d230fb86b04422f583f5893580
Sacha LEFETZ <sacha.lefetz@gmail.com>
2024-07-24 16:05:27 +0200

    header work done

---
+++ digitalchlorine/src/components/WorkComponent.vue
@@ -0,0 +1,45 @@
+<script>
+</script>
+
+<template>
+    <div class="work baserules" id="work">
+        <p class="work--title">WORK.</p>
+        <div class="work__content">
+            <div class="headproject">
+                <div class="headproject__images">
+                    <img src="../assets/images/manjumping3.png" alt="album 1" class="headproject__images--img">
+                    <div class="headproject__images__sub">
+                        <img src="../assets/images/manjumping5.png" alt="album 1" class="headproject__images__sub--img">
+                        <img src="../assets/images/manjumping6.png" alt="album 1" class="headproject__images__sub--img">
+                    </div>
+                </div>
+                <div class="headproject__text">
+                    <p class="headproject__text--title">Meech20x50 - SUICIDE DOORS</p>
+                    <p class="headproject__text--subtitle">alternative cover.</p>
+                    <p class="headproject__text--desc">
+                        I immersed myself in the gritty and intense themes of the song "Suicide Doors" by Meech20x50.
+                        This project is my first fully realized in 3D using Blender, capturing the raw energy and emotional depth of the track through detailed and immersive visuals.
+                    </p>
+                </div>
+            </div>
+
+            <div class="headproject">
+                <div class="headproject__images">
+                    <img src="../assets/images/exitlutherrework10.png" alt="album 1" class="headproject__images--img">
+                    <div class="headproject__images__sub">
+                        <img src="../assets/images/exitlutherrework11.png" alt="album 1" class="headproject__images__sub--img">
+                        <img src="../assets/images/exitlutherrework12.png" alt="album 1" class="headproject__images__sub--img">
+                    </div>
+                </div>
+                <div class="headproject__text">
+                    <p class="headproject__text--title">LUTHER - EXIT</p>
+                    <p class="headproject__text--subtitle">alternative cover.</p>
+                    <p class="headproject__text--desc">
+                        I explored the dark and intense themes of the project. Additionally, I developed two extra variants, each offering a broader perspective on the universe conveyed by the cover. 
+                        These variations aim to enhance the visual and emotional depth, providing a more comprehensive artistic experience.
+                    </p>
+                </div>
+            </div>   
+        </div>
+    </div>
+</template>
--- digitalchlorine/src/views/MainView.vue
+++ digitalchlorine/src/views/MainView.vue
@@ -2,6 +2,7 @@
 import HeaderComponent from '../components/HeaderComponent.vue'
 import HeroBannerComponent from '../components/HeroBannerComponent.vue'
 import AboutComponent from '../components/AboutComponent.vue'
+import WorkComponent from '../components/WorkComponent.vue'
 </script>
 
 <template>
@@ -9,5 +10,6 @@ import AboutComponent from '../components/AboutComponent.vue'
     <main>
         <HeroBannerComponent />
         <AboutComponent />
+        <WorkComponent />
     </main>
 </template>
